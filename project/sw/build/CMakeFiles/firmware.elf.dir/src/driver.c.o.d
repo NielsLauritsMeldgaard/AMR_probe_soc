70b2d7cddd1c0207cd3c2567a77a3abf075137a7
NielsLauritsMeldgaard <62181856+NielsLauritsMeldgaard@users.noreply.github.com>
2026-05-23 15:43:15 +0200

    GIT CHANGE:
    Untracked and added sw/build folder to git ignore

--- project/sw/build/CMakeFiles/firmware.elf.dir/src/driver.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/firmware.elf.dir/src/driver.c.o: \
- /mnt/c/Users/niels/OneDrive/Skrivebord/digital_design_code/Bsc_project/project/sw/src/driver.c \
- /mnt/c/Users/niels/OneDrive/Skrivebord/digital_design_code/Bsc_project/project/sw/src/../inc/hal.h \
- /mnt/c/Users/niels/OneDrive/Skrivebord/digital_design_code/Bsc_project/project/sw/src/../inc/driver.h
--- project/sw/build/CMakeFiles/firmware.elf.dir/src/hal.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/firmware.elf.dir/src/hal.c.o: \
- /mnt/c/Users/niels/OneDrive/Skrivebord/digital_design_code/Bsc_project/project/sw/src/hal.c \
- /mnt/c/Users/niels/OneDrive/Skrivebord/digital_design_code/Bsc_project/project/sw/src/../inc/hal.h
